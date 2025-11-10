47a9a4557de0e3a9066a2249e0fb0302342c9268
haechanmoon <timmymoon@naver.com>
2025-11-10 18:48:01 +0900

    feat(InputView) : 사용자가 '오른손 멜로디'로 변환할 코드 문자열(String)을 입력

--- src/main/java/codemusic/InputView.java
+++ src/main/java/codemusic/InputView.java
@@ -1,5 +1,16 @@
 package codemusic;
 
+import java.util.Scanner;
+
 public class InputView {
-    
+    private final Scanner scanner;
+
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public String getRightHandCode() {
+        System.out.println("오른손 코드를 입력하세요: ");
+        return scanner.nextLine();
+    }
 }
