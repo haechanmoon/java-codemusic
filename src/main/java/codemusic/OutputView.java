fea1815a59b5dc0ef6edaa463e4cb7ebf5a92a8d
haechanmoon <timmymoon@naver.com>
2025-11-13 22:12:12 +0900

    chore(OutputView) : printSetMessage->printStartMessage로 수정

--- src/main/java/codemusic/OutputView.java
+++ src/main/java/codemusic/OutputView.java
@@ -5,7 +5,7 @@ public class OutputView {
         System.out.printf(" 오른손 :[%s] | 왼손 : [%s]%n", rightNote, leftNote);
     }
 
-    public void printSetMessage() {
+    public void printStartMessage() {
         System.out.println("--- 코드를 음악으로 변환합니다. ---");
     }
 }
