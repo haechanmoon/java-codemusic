0c083d6b10e11c49739d8fdeab916547e5253f14
haechanmoon <timmymoon@naver.com>
2025-11-17 21:02:51 +0900

    refactor(Chord) : 화음 무작위 추출 생성

--- src/main/java/codemusic/Chord.java
+++ src/main/java/codemusic/Chord.java
@@ -1,49 +1,71 @@
 package codemusic;
 
+import java.util.Random;
+
 public enum Chord {
-    PUBLIC("public", "C4-E4-G4"),
-    INT("int", "Cffff"),
-    CHAR("char", ""),
-    BOOLEAN("boolean", ""),
-    IF("if", ""),
-    FOR("for", ""),
-    WHILE("while", ""),
-    RETURN("return", ""),
-    CLASS("class", ""),
-    INTERFACE("interface", ""),
-    ABSTRACT("abstract", ""),
-    IMPLEMENTS("implements", ""),
-    NEW("new", ""),
-    THIS("this", ""),
-    SUPER("super", ""),
-    PRIVATE("private", ""),
-    PROTECTED("protected", ""),
-    STATIC("static", ""),
-    FINAL("final", ""),
-    VOID("void", ""),
-    TRY("try", ""),
-    CATCH("catch", ""),
-    THROW("throw", ""),
-    THROWS("throws", ""),
-    IMPORT("import", ""),
-    PACKAGE("package", ""),
-    TRUE("true", ""),
-    FALSE("false", "");
+    PUBLIC("public"),
+    INT("int"),
+    CHAR("char"),
+    BOOLEAN("boolean"),
+    IF("if"),
+    FOR("for"),
+    WHILE("while"),
+    RETURN("return"),
+    CLASS("class"),
+    INTERFACE("interface"),
+    ABSTRACT("abstract"),
+    IMPLEMENTS("implements"),
+    NEW("new"),
+    THIS("this"),
+    SUPER("super"),
+    PRIVATE("private"),
+    PROTECTED("protected"),
+    STATIC("static"),
+    FINAL("final"),
+    VOID("void"),
+    TRY("try"),
+    CATCH("catch"),
+    THROW("throw"),
+    THROWS("throws"),
+    IMPORT("import"),
+    PACKAGE("package"),
+    TRUE("true"),
+    FALSE("false");
 
     private final String keyword;
-    private final String chordNotes;
+    private static final Random random = new Random();
+    private static final int MIN_ROOT_NOTE = 60;
+    private static final int MAX_ROOT_NOTE = 77;
+    private static final String[] NOTE_NAMES = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
 
-    Chord(String keyword, String chordNotes) {
+    Chord(String keyword) {
         this.keyword = keyword;
-        this.chordNotes = chordNotes;
     }
 
     public String getKeyword() {
         return this.keyword;
     }
 
-    public String getChordNotes() {
-        return this.chordNotes;
+    public int[] generateMidiNotes() {
+        int rootNote = MIN_ROOT_NOTE + random.nextInt(MAX_ROOT_NOTE - MIN_ROOT_NOTE + 1);
+        int interval1 = random.nextBoolean() ? 3 : 4;
+        int interval2 = 7 - interval1;
+
+        return new int[]{rootNote, rootNote + interval1, rootNote + interval2};
+    }
+
+    public static String midiArrayToNoteString(int[] midiNumbers) {
+        return String.format("%s-%s-%s",
+                midiToNoteString(midiNumbers[0]),
+                midiToNoteString(midiNumbers[1]),
+                midiToNoteString(midiNumbers[2])
+        );
+    }
+
+    public static String midiToNoteString(int midiNumber) {
+        int octave = (midiNumber / 12) - 1;
+        String note = NOTE_NAMES[midiNumber % 12];
+        return note + octave;
     }
 
     public static Chord findByKeyword(String code) {
