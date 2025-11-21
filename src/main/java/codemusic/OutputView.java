95028081f37f1c1893f95cc29cc1ed22936b28ec
haechanmoon <timmymoon@naver.com>
2025-11-21 22:14:20 +0900

    refactor(OutputView) : 복잡한 루프와 포맷팅 로직 분리

--- src/main/java/codemusic/OutputView.java
+++ src/main/java/codemusic/OutputView.java
@@ -3,44 +3,47 @@ package codemusic;
 import java.util.List;
 
 public class OutputView {
-
     private static final int BEATS_PER_LINE = 6;
     private static final int MAX_NOTE_WIDTH = 10;
+    private static final String MEASURE = " | ";
 
-    /*public void printNoteList(String handName, List<String> notes) {
-        String noteString = String.join("\t| ", notes);
-        System.out.println(handName + ":\t" + noteString);
-    }*/
+    public void printStartMessage() {
+        System.out.println(Messages.START_MSG);
+    }
+
     public void printScore(List<String> rightNotes, List<String> leftNotes, List<String> dynamics) {
+        StringBuilder rightLine = new StringBuilder(Messages.OUTPUT_RIGHT);
+        StringBuilder leftLine = new StringBuilder(Messages.OUTPUT_LEFT);
+        StringBuilder dynamicLine = new StringBuilder(Messages.OUTPUT_DYNAMIC);
 
-        StringBuilder rightLine = new StringBuilder("오른손:\t");
-        StringBuilder leftLine = new StringBuilder("왼손:\t");
-        StringBuilder dynamicLine = new StringBuilder("셈여림:\t");
         for (int i = 0; i < rightNotes.size(); i++) {
-            String rightNote = rightNotes.get(i);
-            String leftNote = leftNotes.get(i);
-            String dynamic = dynamics.get(i);
-
-            String formattedRightNote = String.format("%-" + MAX_NOTE_WIDTH + "s", rightNote);
-            String formattedLeftNote = String.format("%-" + MAX_NOTE_WIDTH + "s", leftNote);
-            String formattedDynamic = String.format("%-" + MAX_NOTE_WIDTH + "s", dynamic);
-
-            rightLine.append(formattedRightNote).append(" | ");
-            leftLine.append(formattedLeftNote).append(" | ");
-            dynamicLine.append(formattedDynamic).append(" | ");
-
-            if ((i + 1) % BEATS_PER_LINE == 0 || (i + 1) == rightNotes.size()) {
-                System.out.print(rightLine + "\n");
-                System.out.print(leftLine + "\n");
-                System.out.print(dynamicLine + "\n");
-                System.out.println();
-
-                rightLine = new StringBuilder("오른손:\t");
-                leftLine = new StringBuilder("왼손:\t");
-                dynamicLine = new StringBuilder("셈여림:\t");
-
-            }
+            appendMeasure(i, rightNotes, leftNotes, dynamics, rightLine, leftLine, dynamicLine);
+            printIfLineEnd(i, rightNotes.size(), rightLine, leftLine, dynamicLine);
         }
+    }
+
+    private void appendMeasure(int i, List<String> rNotes, List<String> lNotes, List<String> dyns,
+                               StringBuilder rLine, StringBuilder lLine, StringBuilder dLine) {
+        rLine.append(format(rNotes.get(i))).append(MEASURE);
+        lLine.append(format(lNotes.get(i))).append(MEASURE);
+        dLine.append(format(dyns.get(i))).append(MEASURE);
+    }
+
+    private void printIfLineEnd(int i, int size, StringBuilder rLine, StringBuilder lLine, StringBuilder dLine) {
+        if ((i + 1) % BEATS_PER_LINE == 0 || (i + 1) == size) {
+            System.out.println(rLine);
+            System.out.println(lLine);
+            System.out.println(dLine);
+            System.out.println();
+
+            resetBuffer(rLine, Messages.OUTPUT_RIGHT);
+            resetBuffer(lLine, Messages.OUTPUT_LEFT);
+            resetBuffer(dLine, Messages.OUTPUT_DYNAMIC);
+        }
+    }
+
+    private String format(String text) {
+        return String.format("%-" + MAX_NOTE_WIDTH + "s", text);
     }
 
     public void printStartMessage() {
