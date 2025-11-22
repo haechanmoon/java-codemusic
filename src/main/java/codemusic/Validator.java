ce0e8da777f225fb9f517e99fc81b2e00f066ca5
haechanmoon <timmymoon@naver.com>
2025-11-22 23:28:33 +0900

    refactor(Validator) : 상수 메세지 관리

--- src/main/java/codemusic/Validator.java
+++ src/main/java/codemusic/Validator.java
@@ -3,7 +3,7 @@ package codemusic;
 public class Validator {
     public static void validateCodeNotEmpty(String code) {
         if (code.isEmpty()) {
-            throw new IllegalArgumentException("[ERROR] 코드는 1글자 이상 입력해야 합니다.");
+            throw new IllegalArgumentException(Messages.ERROR_EMPTY_CODE);
         }
     }
 
